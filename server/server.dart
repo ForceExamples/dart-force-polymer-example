1c4b02c787fd8d0cb2f50cea078c52a6b549e21a
joris hermans <JHermans85@hotmail.com>
2014-05-20 12:48:12 +0200

    remove logging and add logging call from the force framework

--- server/server.dart
+++ server/server.dart
@@ -2,22 +2,14 @@ library example_force_polymer;
 
 import 'dart:async';
 import 'dart:io';
-import 'package:logging/logging.dart' show Logger, Level, LogRecord;
 import 'package:force/force_serverside.dart';
 import 'package:forcemvc/force_mvc.dart';
 import 'package:mustache4dart/mustache4dart.dart'; 
 
 part 'post_controller.dart';
 
-final Logger log = new Logger('ChatApp');
-
 void main() {
-  // Set up logger.
-  Logger.root.level = Level.ALL;
-  Logger.root.onRecord.listen((LogRecord rec) {
-    print('${rec.level.name}: ${rec.time}: ${rec.message}');
-  });
-  
+
 // Setup what port to listen to 
 var portEnv = Platform.environment['PORT'];
 var port = portEnv == null ? 8080 : int.parse(portEnv);
@@ -27,6 +19,10 @@ var serveClient = portEnv == null ? true : false;
 ForceServer fs = new ForceServer(port: port, 
                                 clientFiles: '../client/build/web/',
                                 clientServe: serveClient);
+
+  // Setup logger
+  fs.setupConsoleLog();
+
   if (fs.server.viewRender is MustacheRender) {
     MustacheRender mustacheRender = fs.server.viewRender;
     mustacheRender.delimiter = new Delimiter('[[', ']]');
